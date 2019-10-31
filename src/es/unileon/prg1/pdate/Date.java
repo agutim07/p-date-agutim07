e32d258520b48acaf2e57097d287f7bdabaa41e9
agutim07 <agutim07@estudiantes.unileon.es>
2019-10-31 12:25:54 +0100

    Modificación nº1

--- src/es/unileon/prg1/pdate/Date.java
+++ src/es/unileon/prg1/pdate/Date.java
@@ -1,5 +1,58 @@
 package es.unileon.prg1.pdate;
 
 public class Date {
+	
+	private int day;
+	private int month;
+	private int year;
+	
+	Date() {
+		this.day = 1;
+		this.month = 1;
+		this.year = 2020;
+	}
+	
+	public Date(int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public boolean isSameYear(Date miFecha) {
+		boolean retorno = false;
+		
+		if (this.year==miFecha.year)
+			retorno=true;
+		
+		return retorno;
+	}
+	
+	public boolean isSameMonth(Date miFecha) {
+		boolean retorno = false;
+		
+		if (this.month==miFecha.month)
+			retorno=true;
+		
+		return retorno;
+	}
+	
+	public boolean isSameDay(Date miFecha) {
+		boolean retorno = false;
+		
+		if (this.day==miFecha.day)
+			retorno=true;
+		
+		return retorno;
+	}
+	
+	/*isSameMonth
+	isSameDay
+	isSame*/
 
+	@Override
+	public String toString() {
+		return "Date [day=" + day + ", month=" + month + ", year=" + year + "]";
+	}
+	
 }
+
--- src/es/unileon/prg1/pdate/pdatePrincipal.java
+++ src/es/unileon/prg1/pdate/pdatePrincipal.java
@@ -1,5 +1,26 @@
 package es.unileon.prg1.pdate;
 
 public class pdatePrincipal {
-
+	public static void main(String[] args)	{
+		
+		Date miFecha = new Date();
+		Date miFecha2 = new Date(1,11,2019);
+		System.out.println(miFecha);
+		System.out.println(miFecha2);
+		
+		if(miFecha.isSameYear(miFecha2))
+			System.out.println("Es el mismo año");
+		else
+			System.out.println("No es el mismo año");
+		
+		if(miFecha.isSameMonth(miFecha2))
+			System.out.println("Es el mismo mes");
+		else
+			System.out.println("No es el mismo mes");
+		
+		if(miFecha.isSameDay(miFecha2))
+			System.out.println("Es el mismo día");
+		else
+			System.out.println("No es el mismo día");
+	}
 }
